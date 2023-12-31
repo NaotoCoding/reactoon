6c25a2859b57796bdfad525222dc443b99b63dd8
ynaoto <naoto.zaniah@gmail.com>
2023-12-31 17:43:44 +0900

    ChakraUIを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import GoingAroungClockHand from './components/GoingAroundClockHand';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => (
-  <>
+  <ChakraProvider>
     <Header />
     <GoingAroungClockHand />
-  </>
+  </ChakraProvider>
 );
 
 export default App;
