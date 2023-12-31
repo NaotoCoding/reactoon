f424663bd98f4615d5f669705db52335a61ba285
ynaoto <naoto.zaniah@gmail.com>
2023-12-31 23:49:26 +0900

    画像用カルーセルを作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import UnnecessaryAlert from './components/UnnecessaryAlert';
+import ImagesCarousel from './components/ImagesCarousel';
 
 const App = () => (
   <ChakraProvider>
     <Header />
     <UnnecessaryAlert />
+      <ImagesCarousel width='55%' height='300px'>
+        <img src='react-bootstrap-logo.svg' width='140px' height='140px'/>
+        <img src='antd-logo.svg' width='140px' height='140px'/>
+        <img src='chakra-logo.svg' width='140px' height='140px'/>
+      </ImagesCarousel>
     <GoingAroungClockHand />
   </ChakraProvider>
 );
